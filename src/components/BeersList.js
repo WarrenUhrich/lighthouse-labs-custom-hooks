f2879d41817a5b1e70affed75a9cd20da4cc8ecf
Warren <hello@warren.codes>
2022-03-03 12:37:30 -0700

    Add localstorage for pageNumber in BeersList component.

--- src/components/BeersList.js
+++ src/components/BeersList.js
@@ -1,9 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useFetch from '../hooks/useFetch';
 import useLogChanges from '../hooks/useLogChanges';
 
 export default function BeersList() {
-    const [pageNumber, setPageNumber] = useState(1);
+    const [pageNumber, setPageNumber] = useState(localStorage.getItem('pageNumber'));
+    useEffect(()=>{
+        localStorage.setItem('pageNumber', pageNumber);
+    }, [pageNumber]);
+
     const [beers, status, error] = useFetch('https://api.punkapi.com/v2/beers?page='+pageNumber+'&per_page=16');
     useLogChanges(beers);
 
@@ -11,7 +15,7 @@ export default function BeersList() {
         <>
             <h2>Beers List (Page {pageNumber})</h2>
             {(pageNumber > 1) && <button onClick={e => {setPageNumber(pageNumber - 1)}}>Previous</button>}
-            <button onClick={e => {setPageNumber(pageNumber + 1)}}>Next</button>
+            <button onClick={e => {setPageNumber(Number(pageNumber) + 1)}}>Next</button>
             <ul>
                 {error && <li>{error}</li>}
                 {status && <li>{status}</li>}
