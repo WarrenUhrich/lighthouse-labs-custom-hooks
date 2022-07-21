a8614692240cb8f546eac22cd548d40a143a2035
Warren <hello@warren.codes>
2022-07-21 11:12:21 -0600

    Add console log to Counter.

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,12 @@
-import {useState} from 'react';
+import {useState, useEffect } from 'react';
 
 const Counter = () => {
     const [count, setCount] = useState(0);
     // returns an array: [value, functionForUpdating()]
+
+    useEffect(() => {
+        console.log('Count is: ', count);
+    }, [count]);
 
     return (
         <button onClick={event => {setCount(prev => prev + 1)}}>
